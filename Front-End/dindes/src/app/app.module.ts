ebe14411485ddd15162a732537dd862fb43004f4
Alex <alourenco08@gmail.com>
2021-07-01 10:47:20 -0300

    Service e environment

--- Front-End/dindes/src/app/app.module.ts
+++ Front-End/dindes/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import {HttpClientModule} from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -26,7 +27,8 @@ import { SobreComponent } from './sobre/sobre.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
